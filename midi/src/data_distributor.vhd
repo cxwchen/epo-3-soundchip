849c1e13928029d09b0bf490b01bb6fe71e65784
cxwchen <100145127+cxwchen@users.noreply.github.com>
2022-12-06 10:13:45 +0100

    start of distributor; minor changes fsm
    errors due to typos fixed (fsm)

--- midi/src/data_distributor.vhd
+++ midi/src/data_distributor.vhd
@@ -1,4 +1,4 @@
-library library IEEE;
+library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
@@ -18,3 +18,42 @@ entity distributor is
 end entity distributor;
 
 architecture behavioural of distributor is
+    type distributor_state_type is (reset_state, note_off_state, note_on_state);
+    signal distributor_state, new_distributor_state : distributor_state_type;
+
+    begin
+        process(clk, reset)
+        begin
+            if rising_edge(clk) then
+                if (reset='1') then
+                    distributor_state <= reset_state;
+
+                else
+                    distributor_state <= new_distributor_state;
+                end if;
+            end if;
+        end process;
+
+        process(distributor_state, SD_in, clk)
+        begin
+            case distributor_state is
+                when reset_state =>
+                    pitch_TG0 <= (others=>'0');
+                    pitch_TG1 <= (others=>'0');
+                    pitch_TG2 <= (others=>'0');
+                    note_on <= (others => '0');
+                    velo0 <= (others => '0');
+                    velo1 <= (others => '0');
+                    velo2 <= (others => '0');
+                    velo3 <= (others => '0');
+                    if (SD_in(6 downto 4) = "000") then
+                        new_distributor_state <= note_off_state;
+                    elsif (SD_in(6 downto 4) = "001") then
+                        new_distributor_state <= note_on_state;
+                    else
+                        new_distributor_state <= reset_state;
+                    end if;
+                when note_on_state =>
+                    --- hier moet een bepaalde pitch range naar een bepaalde tone generator, die verdeling moet nog gemaakt worden
+                    -- aan de hand van welke range van keys naar welke tone generator moet, weten we ook welke output module daarvoor gebruikt moet worden
+
--- midi/src/fsm.vhdl
+++ midi/src/fsm.vhdl
@@ -1,11 +1,11 @@
-library library IEEE;
+library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity fsm is
     port (
         clk, reset  : in std_logic;
-        count       : in std_logic_vector(9 downtwo 0);
+        count       : in std_logic_vector(9 downto 0);
         ready       : in std_logic;
         Din         : in std_logic;
         count_reset : out std_logic;
@@ -14,7 +14,7 @@ entity fsm is
 end entity fsm;
 
 architecture behavioural of fsm is
-    type fsm_state_type is (idle, start, read, write, final_read, final_write);
+    type fsm_state_type is (idle, start, read_state, write_state, final_read, final_write);
     signal fsm_state, new_fsm_state : fsm_state_type;
 
 begin
@@ -34,50 +34,50 @@ begin
     begin            
         case fsm_state is
             when idle =>
-                enable = '0';
-                count_reset = '0';
+                enable <= '0';
+                count_reset <= '0';
                 if (Din = '1') then
                     new_fsm_state <= idle;
 
-                if (Din = '0') then
+                elsif (Din = '0') then
                     new_fsm_state <= start;
                 end if;
 
             when start =>
-                enable = '0';
-                count_reset = '0';
+                enable <= '0';
+                count_reset <= '0';
                 if (unsigned(count)>=to_unsigned(400,10)) then
-                    new_fsm_state <= read;
+                    new_fsm_state <= read_state;
                 else
                     new_fsm_state <= start;
                 end if;
 
-            when read =>   
-                enable = '0';
-                count_reset = '1';
+            when read_state =>   
+                enable <= '0';
+                count_reset <= '1';
                 if (unsigned(count)>=(to_unsigned(800, 10))) then
-                    new_fsm_state <= write;
+                    new_fsm_state <= write_state;
                 end if;
 
-            when write =>
-                enable = '1';
-                count_reset = '1';
+            when write_state =>
+                enable <= '1';
+                count_reset <= '1';
                 if (ready = '1') then
                     new_fsm_state <= final_read;
                 else 
-                    new_fsm_state <= read;
+                    new_fsm_state <= read_state;
                 end if;
 
             when final_read =>
-                enable = '0';
-                count_reset = '1';
+                enable <= '0';
+                count_reset <= '1';
                 if (unsigned(count)>=(to_unsigned(800, 10))) then
                     new_fsm_state <= final_write;
                 end if;
             
             when final_write =>
-                enable = '1';
-                count_reset = '1';
+                enable <= '1';
+                count_reset <= '1';
                 new_fsm_state <= idle;                
         end case;
     end process;
