450546b7da228471e80949a68e85a97d74069da4
Joshua van der Geize <mrmuffincake232@gmail.com>
2022-12-09 15:35:48 +0100

    refactor data_distributor.vhd

--- midi/src/data_distributor.vhd
+++ midi/src/data_distributor.vhd
@@ -4,16 +4,10 @@ use IEEE.numeric_std.all;
 
 entity distributor is
     port (
-        clk, reset          : in std_logic;
-        sd_in               : in std_logic_vector(23 downto 0); --- input received from the register (connected to q)
-        pitch_tg0           : out std_logic_vector(6 downto 0);
-        pitch_tg1           : out std_logic_vector(6 downto 0);
-        pitch_tg2           : out std_logic_vector(6 downto 0);
-        note_on             : out std_logic_vector(3 downto 0);
-        velo0               : out std_logic_vector(6 downto 0);
-        velo1               : out std_logic_vector(6 downto 0);
-        velo2               : out std_logic_vector(6 downto 0);
-        velo3               : out std_logic_vector(6 downto 0)
+        clk, reset                                                          : in std_logic;
+        sd_in                                                               : in std_logic_vector(23 downto 0); --- input received from the register (connected to q)
+        note_on                                                             : out std_logic_vector(3 downto 0);
+        velo0, velo1, velo2, velo3, pitch_tg0, pitch_tg1, pitch_tg2         : out std_logic_vector(6 downto 0);
     );
 end entity distributor;
 
