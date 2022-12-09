dd4d0ac483eac66c785f5332d52e5f32eefa6236
Muffinrl <mrmuffincake232@gmail.com>
2022-12-09 14:39:26 +0100

    zucht

--- midi/src/fsm.vhd
+++ midi/src/fsm.vhd
@@ -1,11 +1,11 @@
-library library IEEE;
+library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity fsm is
     port (
         clk, reset  : in std_logic;
-        count       : in std_logic_vector(9 downtwo 0);
+        count       : in std_logic_vector(9 downto 0);
         ready       : in std_logic;
         Din         : in std_logic;
         count_reset : out std_logic;
@@ -34,18 +34,17 @@ begin
     begin            
         case fsm_state is
             when idle =>
-                enable = '0';
-                count_reset = '0';
+                enable <= '0';
+                count_reset <= '0';
                 if (Din = '1') then
                     new_fsm_state <= idle;
-
-                if (Din = '0') then
+                elsif (Din = '0') then
                     new_fsm_state <= start;
                 end if;
 
             when start =>
-                enable = '0';
-                count_reset = '0';
+                enable <= '0';
+                count_reset <= '0';
                 if (unsigned(count)>=to_unsigned(400,10)) then
                     new_fsm_state <= read;
                 else
@@ -53,15 +52,15 @@ begin
                 end if;
 
             when read =>   
-                enable = '0';
-                count_reset = '1';
+                enable <= '0';
+                count_reset <= '1';
                 if (unsigned(count)>=(to_unsigned(800, 10))) then
                     new_fsm_state <= write;
                 end if;
 
             when write =>
-                enable = '1';
-                count_reset = '1';
+                enable <= '1';
+                count_reset <= '1';
                 if (ready = '1') then
                     new_fsm_state <= final_read;
                 else 
@@ -69,15 +68,15 @@ begin
                 end if;
 
             when final_read =>
-                enable = '0';
-                count_reset = '1';
+                enable <= '0';
+                count_reset <= '1';
                 if (unsigned(count)>=(to_unsigned(800, 10))) then
                     new_fsm_state <= final_write;
                 end if;
             
             when final_write =>
-                enable = '1';
-                count_reset = '1';
+                enable <= '1';
+                count_reset <= '1';
                 new_fsm_state <= idle;                
         end case;
     end process;
