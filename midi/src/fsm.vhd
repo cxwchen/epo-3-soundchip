baf54f09704626e090ecf49ebc40f63e1e84a230
Joshua van der Geize <mrmuffincake232@gmail.com>
2022-12-16 14:14:58 +0100

    FSM edits complete, time for bug fixing

--- midi/src/fsm.vhd
+++ midi/src/fsm.vhd
@@ -4,18 +4,15 @@ use IEEE.numeric_std.all;
 
 entity fsm is
     port (
-        clk, reset  : in std_logic;
-        count       : in std_logic_vector(9 downto 0);
-        ready       : in std_logic_vector(2 downto 0);
-        Din         : in std_logic;
-        count_reset : out std_logic;
-        reg_reset   : out std_logic;
-        enable      : out std_logic
+        clk, reset, Din                 : in std_logic;
+        count                           : in std_logic_vector(9 downto 0);
+        ready                           : in std_logic_vector(2 downto 0);
+        count_reset, reg_reset, enable  : out std_logic
     );
 end entity fsm;
 
 architecture behavioural of fsm is
-    type fsm_state_type is (rest, idle, init, start, read_status, read_data_1, read_data_2, write_status, write_data_1, write_data_2);
+    type fsm_state_type is (rest, idle, offset, start, init_status, init_data, read_status, read_data_1, read_data_2, write_status, write_data_1, write_data_2);
     signal fsm_state, new_fsm_state : fsm_state_type;
 
 begin
@@ -41,7 +38,7 @@ begin
                 reg_reset       <= '1';
 
                 new_fsm_state   <= idle;
-            -- Idle waits for input D_in to go to 0, then moves to init.    
+            -- Idle waits for input D_in to go to 0, then moves to offset.    
             when idle =>
                 enable          <= '0';
                 count_reset     <= '1';
@@ -52,33 +49,45 @@ begin
                 elsif (Din = '0') then
                     new_fsm_state <= init;
                 end if;
-            -- Init is only ever active for one tick, stores the first 0 bit into the register.
-            when init =>
-                enable          <= '1';
-                count_reset     <= '1';
+            
+            -- Offset counts to 400 (ticks) to offset the data sampling by half a period (T = 800 ticks).
+            when offset =>
+                enable          <= '0';
+                count_reset     <= '0';
                 reg_reset       <= '0';
 
-                new_fsm_state   <= start;
-        
-            -- Start counts to 400 to offset the data sampling by half a period (T = 800), next state is based off of which registers are ready.
+                if (unsigned(count)>=to_unsigned(400,10)) then
+                    new_fsm_state   <= start;
+                else
+                    new_fsm_state   <= offset;
+                end if;
+
+            -- Start counts to 800, next state is based off of which registers are ready.
             when start =>
                 enable          <= '0';
                 count_reset     <= '0';
                 reg_reset       <= '0';
-                if (unsigned(count)>=to_unsigned(400,10)) then
-                    case ready is
-                        when "000"  =>
-                            new_fsm_state   <= read_status;
-                        when "001"  =>
-                            new_fsm_state   <= read_data_1;
-                        when "011"  =>
-                            new_fsm_state   <= read_data_2;
-                        when others =>
-                            new_fsm_state   <= start;
-                else
-                    new_fsm_state <= start;
-                end if;
 
+                if (unsigned(count)>=to_unsigned(800,10)) then
+                    if (Din = '1') then
+                        new_fsm_state   <= init_status;
+                    else
+                        if (ready(0) = '1') then
+                            new_fsm_state   <= init_data;
+                        else -- Ignore data byte if there is no status set
+                            new_fsm_state   <= rest;
+                    end if;
+                else
+                    new_fsm_state   <= start;
+                end if;
+            
+            when init_status =>
+                enable          <= '0';
+                count_reset     <= '1';
+                reg_reset       <= '1';
+
+                new_fsm_state   <= read_status;
+            
             -- Read_Status is part of a Read/Write loop that should occur 8 times total.
             when read_status =>   
                 enable          <= '0';
@@ -102,6 +111,20 @@ begin
                     new_fsm_state <= idle;
                 else 
                     new_fsm_state <= read_status;
+                end if;
+
+            -- Init_Data resets the counter, then continues to either R_D_1 or R_D_2 based on the ready state(s) of the register.
+            when init_data =>
+                enable          <= 0;
+                count_reset     <= 1;
+                reg_reset       <= 0;
+
+                if (ready = "001") then
+                    new_fsm_state   <= read_data_1;
+                elsif(ready = "011") then
+                    new_fsm_state   <= read_data_2;
+                else -- Error
+                    new_fsm_state   <= rest;
                 end if;
 
             -- Read_Data_1 is part of a Read/Write loop that should occur 8 times total.
