3a84450ce290eb880152641fd534fa7ba62082e1
Joshua van der Geize <mrmuffincake232@gmail.com>
2022-12-23 16:10:35 +0100

    Fixes, integrating all submodules

--- midi/src/counter.vhd
+++ midi/src/counter.vhd
@@ -5,8 +5,8 @@ use IEEE.numeric_std.all;
 -- MIDI Baud rate: 31250bps; 25MHz/31250 = 800
 entity counter is
     port (
-        clk, reset          : in std_logic;
-        count_out           : out std_logic_vector(9 downto 0)
+        clk, reset, loc_reset   : in std_logic;
+        count_out               : out std_logic_vector(9 downto 0)
     );
 end entity counter;
 
@@ -15,10 +15,9 @@ architecture structural of counter is
     signal count, new_count     : unsigned(9 downto 0);
 
 begin
-
     timebase : process(clk, reset)
 	begin
-        if reset = '1' then
+        if ( reset = '1' or loc_reset = '1' ) then
             count <= (others => '0');
         elsif rising_edge(clk) then
             count <= new_count;
