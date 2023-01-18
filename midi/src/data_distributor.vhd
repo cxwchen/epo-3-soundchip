6e29c388ea3b7566cedcddd0d6bad3a336e331f9
Joshua van der Geize <mrmuffincake232@gmail.com>
2023-01-18 12:56:35 +0100

    data_distributor formal FSM form fix

--- midi/src/data_distributor.vhd
+++ midi/src/data_distributor.vhd
@@ -13,6 +13,9 @@ entity distributor is
 end entity distributor;
 
 architecture behavioural of distributor is
+    type fsm_state_type is (rst, idle, read, update);
+
+    signal fsm_state, new_fsm_state     : fsm_state_type;
 
     -- Internal signals representing outputs.
     signal notes, new_notes             : std_logic_vector(3 downto 0);
@@ -21,86 +24,208 @@ architecture behavioural of distributor is
     
 
 begin
-    process(clk, reset)
+    -- process(clk, reset)
+    -- begin
+    --     if (reset = '1' or loc_reset = '1') then
+    --         notes           <= (others => '0');
+    --         new_notes       <= (others => '0');
+    --         pitches         <= (others => '0');
+    --         new_pitches     <= (others => '0');
+    --         velocities      <= (others => '0');
+    --         new_velocities  <= (others => '0');
+    --     elsif (clk'event and clk = '1') then
+    --         -- Update current signal on clock cycle.
+    --         notes       <= new_notes;
+    --         pitches     <= new_pitches;
+    --         velocities  <= new_velocities;
+    --         -- TODO: A dedicated noise note/key needs to be chosen
+    --         -- Check if the data is ready to be updated
+    --         if (reg_ready = "111") then
+    --             if (sd_in(6 downto 4) = "001") then -- "001" means that the status message is a Note On message
+    --                 if ((unsigned(sd_in(14 downto 8)) <= to_unsigned(48,7)) and (unsigned(sd_in(14 downto 8)) >= to_unsigned(21,7))) then -- Check if the input note is in the bass register (C0 - C3)
+    --                     new_pitches(6 downto 0)         <= sd_in(14 downto 8);
+    --                     new_pitches(20 downto 7)        <= new_pitches(20 downto 7);
+
+    --                     new_notes(0)                    <= '1';
+    --                     new_notes(2 downto 1)           <= new_notes(2 downto 1);
+
+    --                     new_velocities(6 downto 0)      <= sd_in(22 downto 16);
+    --                     new_velocities(20 downto 7)     <= new_velocities(20 downto 7);
+    --                 elsif((unsigned(sd_in(14 downto 8)) <= to_unsigned(108,7)) and (unsigned(sd_in(14 downto 8)) >= to_unsigned(81,7))) then -- High
+    --                     new_pitches(20 downto 14)       <= sd_in(14 downto 8);
+    --                     new_pitches(13 downto 0)        <= new_pitches(13 downto 0);
+
+    --                     new_notes(2)                    <= '1';
+    --                     new_notes(1 downto 0)           <= new_notes(1 downto 0);
+
+    --                     new_velocities(20 downto 14)    <= sd_in(22 downto 16);
+    --                     new_velocities(13 downto 0)     <= new_velocities(13 downto 0);
+    --                 else -- Mid
+    --                     new_pitches(13 downto 7)        <= sd_in(14 downto 8);
+    --                     new_pitches(20 downto 14)       <= new_pitches(20 downto 14);
+    --                     new_pitches(6 downto 0)         <= new_pitches(6 downto 0);
+
+    --                     new_notes(1)                    <= '1';
+    --                     new_notes(2)                    <= new_notes(2);
+    --                     new_notes(0)                    <= new_notes(0);
+
+    --                     new_velocities(20 downto 14)    <= new_velocities(20 downto 14);
+    --                     new_velocities(13 downto 7)     <= sd_in(22 downto 16);
+    --                     new_velocities(6 downto 0)      <= new_velocities(6 downto 0);
+
+    --                 -- elsif ( sd_in(14 downto 8) >= ('0' & pitches(13 downto 7)) ) then -- Check if the new input is a higher pitch than the current note on TG1
+    --                 --     new_pitches(20 downto 14)       <= sd_in(14 downto 8);
+    --                 --     new_notes(2)                    <= '1';
+    --                 --     new_velocities(20 downto 14)    <= sd_in(6 downto 0);
+    --                 -- else
+    --                 --     new_pitches(13 downto 7)    <= sd_in(14 downto 8); -- If the pitch isnt the current highest or a bass note, route it to TG1
+    --                 --     new_notes(1)                <= '1';
+    --                 --     new_velocities(13 downto 7) <= sd_in(6 downto 0);
+    --                 end if;
+    --             elsif (sd_in(6 downto 4) = "000") then -- "000" means that the status message is a Note off message
+    --                 if (sd_in(15 downto 8) = ('0' & pitches(6 downto 0)) ) then
+    --                     -- new_pitches(6 downto 0) <= (others => '0');
+    --                     new_notes(0)            <= '0';
+    --                     new_notes(2 downto 1)   <= new_notes(2 downto 1);
+
+    --                 elsif (sd_in(15 downto 8) = ('0' & pitches(13 downto 7)) ) then
+    --                     -- new_pitches(13 downto 7) <= (others => '0');
+    --                     new_notes(0)            <= new_notes(0);
+    --                     new_notes(1)            <= '0';
+    --                     new_notes(2)            <= new_notes(2);
+
+    --                 elsif (sd_in(15 downto 8) = ('0' & pitches(20 downto 14)) ) then
+    --                     -- new_pitches(20 downto 14) <= (others => '0');
+    --                     new_notes(2)            <= '0';
+    --                     new_notes(1 downto 0)   <= new_notes(1 downto 0);
+                        
+    --                 else
+    --                     new_notes       <= new_notes;
+    --                 end if;
+    --             else
+    --                 new_pitches         <= new_pitches;
+    --                 new_notes           <= new_notes;
+    --                 new_velocities      <= new_velocities;
+    --             end if; 
+    --         end if;
+    --     end if;
+    -- end process;
+
+    reg : process( clk, reset, loc_reset )
     begin
         if (reset = '1' or loc_reset = '1') then
-            notes           <= (others => '0');
-            new_notes       <= (others => '0');
-            pitches         <= (others => '0');
-            new_pitches     <= (others => '0');
-            velocities      <= (others => '0');
-            new_velocities  <= (others => '0');
-        elsif (clk'event and clk = '1') then
-            -- Update current signal on clock cycle.
-            notes       <= new_notes;
-            pitches     <= new_pitches;
-            velocities  <= new_velocities;
-            -- TODO: A dedicated noise note/key needs to be chosen
-            -- Check if the data is ready to be updated
-            if (reg_ready = "111") then
+            fsm_state   <= rst;
+        elsif( clk'event and clk = '1') then
+            fsm_state   <= new_fsm_state;
+        end if;
+    end process ; -- reg
+
+    comb : process( sd_in, reg_ready, notes, new_notes, pitches, new_pitches, velocities, new_velocities, fsm_state)
+    begin
+        case fsm_state is
+            when rst   =>
+                notes           <= (others  => '0');
+                new_notes       <= (others  => '0');
+                pitches         <= (others  => '0');
+                new_pitches     <= (others  => '0');
+                velocities      <= (others  => '0');
+                new_velocities  <= (others  => '0');
+
+                if (reset = '0' and loc_reset = '0') then
+                    new_fsm_state   <= idle;
+                else
+                    new_fsm_state   <= rst;
+                end if;
+
+            when idle   =>
+                notes           <= notes;
+                pitches         <= pitches;
+                velocities      <= velocities;
+                new_notes       <= new_notes;
+                new_pitches     <= new_pitches;
+                new_velocities  <= new_velocities;
+
+                if(reg_ready = "111") then
+                    new_fsm_state   <= read;
+                else
+                    new_fsm_state   <= idle;
+                end if;
+
+            when read   =>
+                notes           <= notes;
+                pitches         <= pitches;
+                velocities      <= velocities;
+                
+                new_fsm_state   <= update;
+                
                 if (sd_in(6 downto 4) = "001") then -- "001" means that the status message is a Note On message
                     if ((unsigned(sd_in(14 downto 8)) <= to_unsigned(48,7)) and (unsigned(sd_in(14 downto 8)) >= to_unsigned(21,7))) then -- Check if the input note is in the bass register (C0 - C3)
-                        new_pitches(6 downto 0)     <= sd_in(14 downto 8);
-                        new_notes(0)                <= '1';
-                        new_velocities(6 downto 0)  <= sd_in(22 downto 16);
+                        new_pitches(6 downto 0)         <= sd_in(14 downto 8);
+                        new_pitches(20 downto 7)        <= new_pitches(20 downto 7);
+
+                        new_notes(0)                    <= '1';
+                        new_notes(2 downto 1)           <= new_notes(2 downto 1);
+
+                        new_velocities(6 downto 0)      <= sd_in(22 downto 16);
+                        new_velocities(20 downto 7)     <= new_velocities(20 downto 7);
                     elsif((unsigned(sd_in(14 downto 8)) <= to_unsigned(108,7)) and (unsigned(sd_in(14 downto 8)) >= to_unsigned(81,7))) then -- High
                         new_pitches(20 downto 14)       <= sd_in(14 downto 8);
-                        new_notes(2)                    <= '1';
-                        new_velocities(20 downto 14)    <= sd_in(22 downto 16);
-                    else -- Mid
-                        new_pitches(13 downto 7)    <= sd_in(14 downto 8);
-                        new_notes(1)                <= '1';
-                        new_velocities(13 downto 7) <= sd_in(22 downto 16);
+                        new_pitches(13 downto 0)        <= new_pitches(13 downto 0);
 
-                    -- elsif ( sd_in(14 downto 8) >= ('0' & pitches(13 downto 7)) ) then -- Check if the new input is a higher pitch than the current note on TG1
-                    --     new_pitches(20 downto 14)       <= sd_in(14 downto 8);
-                    --     new_notes(2)                    <= '1';
-                    --     new_velocities(20 downto 14)    <= sd_in(6 downto 0);
-                    -- else
-                    --     new_pitches(13 downto 7)    <= sd_in(14 downto 8); -- If the pitch isnt the current highest or a bass note, route it to TG1
-                    --     new_notes(1)                <= '1';
-                    --     new_velocities(13 downto 7) <= sd_in(6 downto 0);
+                        new_notes(2)                    <= '1';
+                        new_notes(1 downto 0)           <= new_notes(1 downto 0);
+
+                        new_velocities(20 downto 14)    <= sd_in(22 downto 16);
+                        new_velocities(13 downto 0)     <= new_velocities(13 downto 0);
+                    else -- Mid
+                        new_pitches(13 downto 7)        <= sd_in(14 downto 8);
+                        new_pitches(20 downto 14)       <= new_pitches(20 downto 14);
+                        new_pitches(6 downto 0)         <= new_pitches(6 downto 0);
+
+                        new_notes(1)                    <= '1';
+                        new_notes(2)                    <= new_notes(2);
+                        new_notes(0)                    <= new_notes(0);
+
+                        new_velocities(20 downto 14)    <= new_velocities(20 downto 14);
+                        new_velocities(13 downto 7)     <= sd_in(22 downto 16);
+                        new_velocities(6 downto 0)      <= new_velocities(6 downto 0);
                     end if;
-                elsif (sd_in(22 downto 20) = "000") then -- "000" means that the status message is a Note off message
+                elsif (sd_in(6 downto 4) = "000") then -- "000" means that the status message is a Note off message
                     if (sd_in(15 downto 8) = ('0' & pitches(6 downto 0)) ) then
                         -- new_pitches(6 downto 0) <= (others => '0');
-                        new_notes(0)    <= '0';
+                        new_notes(0)            <= '0';
+                        new_notes(2 downto 1)   <= new_notes(2 downto 1);
+
                     elsif (sd_in(15 downto 8) = ('0' & pitches(13 downto 7)) ) then
                         -- new_pitches(13 downto 7) <= (others => '0');
-                        new_notes(1)    <= '0';
+                        new_notes(0)            <= new_notes(0);
+                        new_notes(1)            <= '0';
+                        new_notes(2)            <= new_notes(2);
+
                     elsif (sd_in(15 downto 8) = ('0' & pitches(20 downto 14)) ) then
                         -- new_pitches(20 downto 14) <= (others => '0');
-                        new_notes(2)    <= '0';
+                        new_notes(2)            <= '0';
+                        new_notes(1 downto 0)   <= new_notes(1 downto 0);
+                    else
+                        new_notes       <= new_notes;
                     end if;
+                else
+                    new_pitches         <= new_pitches;
+                    new_notes           <= new_notes;
+                    new_velocities      <= new_velocities;
                 end if; 
-            end if;
-        end if;
 
-        -- if (sd_in(6 downto 4) = "000") or (sd_in(6 downto 4) = "001") then --- note off or note on message
-        --     if (unsigned(sd_in(15 downto 8)) <= to_unsigned(.....)) and (unsigned(sd_in(15 downto 8)) >= to_unsigned(....)) then --- low: if the pitch is between a certain range (has not been decided yet) the tone generator responsible for that pitch needs to receive the note number (data byte 1) and the corresponding output module needs to receive the velocity (data byte 2)
-        --         pitch_tg0 <= sd_in(15 downto 8);
-        --         velo0 <= sd_in(23 downto 16);
-        --     elsif (unsigned(sd_in(15 downto 8)) <= to_unsigned(.....)) and (unsigned(sd_in(15 downto 8)) >= to_unsigned(.....)) then --- mid: different range, see above for an explanation
-        --         pitch_tg1 <= sd_in(15 downto 8);
-        --         velo1 <= sd_in(23 downto 16);
-        --     elsif (unsigned(sd_in(15 downto 8)) <= to_unsigned(....)) and (unsigned(sd_in(15 downto 9)) >= to_unsigned(....)) then --- high: different range, see above for an explanation
-        --         pitch_tg2 <= sd_in(15 downto 8);
-        --         velo2 <= sd_in(23 downto 16);
-        --     else -- the note number is outside of the ranges described above so it will be considered noise
-        --         --- hier weet ik niet precies wat ik moet doen voor de tone generator, want het note_on signaal wordt daarvoor gebruikt
-        --         velo3 <= sd_in(23 downto 16);
-        --     end if;
-        -- else --- a status message we ignore 
-        --     pitch_tg0   <= (others=>'0');
-        --     pitch_tg1   <= (others=>'0');
-        --     pitch_tg2   <= (others=>'0');
-        --     note_on     <= (others=>'0');
-        --     velo0       <= (others=>'0');
-        --     velo1       <= (others=>'0');
-        --     velo2       <= (others=>'0');
-        --     velo3       <= (others=>'0');
-        -- end if;
-    end process;
+            when update =>
+                notes           <= new_notes;
+                pitches         <= new_pitches;
+                velocities      <= new_velocities;
+                new_notes       <= new_notes;
+                new_pitches     <= new_pitches;
+                new_velocities  <= new_velocities;
+
+                new_fsm_state   <= idle;
+        end case;
+    end process ; -- comb
 
     note_on     <= notes;
 
